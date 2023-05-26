06e28e846eb9a657f136b8ac57b2f493ca2d1269
choi4624 <choi4624@gmail.com>
2023-05-26 15:52:27 +0900

    pycam test

--- pycam/pycam.py
+++ pycam/pycam.py
@@ -4,12 +4,15 @@ from picamera2 import Picamera2, Preview
 picam = Picamera2()
 
 config = picam.create_preview_configuration()
+config.resolution = (640, 480)
+config.framerate = 15
+
 picam.configure(config)
 
-picam.start_preview(Preview.QTGL)
-
 picam.start()
-time.sleep(2)
-picam.capture_file("test-python.jpg")
+
+while True:
+    time.sleep(60)
+    picam.capture_file("/mnt/dsm/temp/test-python-%s.jpg" % time.time())
 
 picam.close()
