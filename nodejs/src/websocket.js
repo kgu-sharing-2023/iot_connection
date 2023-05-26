58f1fc9ad472aa8a96f9216c57090c9067053964
choi4624 <choi4624@gmail.com>
2023-05-26 14:44:01 +0900

    on off 구현

--- nodejs/src/websocket.js
+++ nodejs/src/websocket.js
@@ -1,12 +1,11 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
-var url = requir('url');
-var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+var url = require('url');
+
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
+var LED = new Gpio(5, 'out'); //use GPIO pin 4 as output
 var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
-http.on('listening');
 http.listen(28080); //listen to port 28080
 
 function handler (req, res) { //create server
@@ -58,6 +57,27 @@ function handler (req, res) { //create server
         return res.end();
       });
     }
+
+    else if (req.url == '/on') {
+      fs.readFile(__dirname + '/resources/web/gpio.html', function(err, data){
+      LED.writeSync(1); //set pin state to 1 (turn LED on);
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      console.log("led on");
+      return res.end();
+    });
+    }
+  
+
+    else if (req.url == '/off') {
+      fs.readFile(__dirname + '/resources/web/gpio.html', function(err, data){
+      LED.writeSync(0); //set pin state to 1 (turn LED on);
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      console.log("led off");
+      return res.end();
+    });
+  }
+
+
   }
 
   else if (req.method === 'POST') {
@@ -65,46 +85,19 @@ function handler (req, res) { //create server
       let pin = '';
     }
 
-    else if (req.url == '/comp') {
-      
-    }
+    
+  
   }
 }
 
-function handler (req, res) { //create server
-  fs.readFile(__dirname + '/resources/web/index.html', function(err, data) { //read file index.html in public folder
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
-  });
-}
-
-io.sockets.on('connection', function (socket) {// WebSocket Connection
-  var lightvalue = 0; //static variable for current status
-  pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton
-    if (err) { //if an error
-      console.error('There was an error', err); //output error message to console
-      return;
-    }
-    console.log("io switch work");
-    lightvalue = value;
-    socket.emit('light', lightvalue); //send button status to client
-  });
-  socket.on('light', function(data) { //get light switch status from client
-    lightvalue = data;
-    if (lightvalue != LED.readSync()) { //only change LED if status has changed
-      LED.writeSync(lightvalue); //turn LED on or off
-    }
-  });
-});
-
-process.on('SIGINT', function () { //on ctrl+c
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport LED GPIO to free resources
-  pushButton.unexport(); // Unexport Button GPIO to free resources
-  process.exit(); //exit completely
-});
+// function handler (req, res) { //create server
+//   fs.readFile(__dirname + '/resources/web/menu.html', function(err, data) { //read file index.html in public folder
+//     if (err) {
+//       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+//       return res.end("404 Not Found");
+//     }
+//     res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+//     res.write(data); //write data from index.html
+//     return res.end();
+//   });
+// }
