a646f2276773e65d481d123f3e630c592bb7502c
choi4624 <choi4624@gmail.com>
2023-06-08 20:40:57 +0900

    adjust mp4_capture.py from picamera2 examples

--- pycam/pycam.py
+++ pycam/pycam.py
@@ -1,22 +1,23 @@
 import time
 from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
 
-picam = Picamera2()
+picam2 = Picamera2()
 
-picam.resolution = (640, 480)
-picam.framerate = 15
+config = picam2.create_video_configuration(main={"size": (1280, 720)})
+picam2.configure(config)
 
 encoder = H264Encoder(10000000)
-
-picam.start_recording(encoder, "/mnt/dsm/temp/test-python-%s.mkv" % time.time())
+output = FfmpegOutput("/mnt/dsm/temp/test-python-%s.mp4" % time.time())
 
 while True:
     # 60초마다 영상을 끊고 파일을 저장하는 반복문
     time.sleep(60)
 
-    picam.stop_recording()
+    picam2.stop_recording()
 
-    picam.start_recording("/mnt/dsm/temp/test-python-%s.mkv" % time.time(), format="h264")
+    picam2.start_recording(encoder, output )
 
 picam.stop_recording()
 
