82fcacf6b2871235caa11a8dc12e3c815cdf59cf
choi4624 <choi4624@gmail.com>
2023-06-08 21:44:32 +0900

    1st try suceess

--- pycam/pycam.py
+++ pycam/pycam.py
@@ -1,23 +1,39 @@
 import time
+import datetime
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
+import os
+import shutil
 
-picam2 = picamera2()
+# Set the desired working directory
+print(os.getcwd())
+working_directory = "/home/kgu4624/Documents/nodegpio/webdav"
+dst = "/mnt/dsm/temp/_cctv/"
+os.chdir(working_directory)
 
-config = picam2.create_video_configuration(main={"size": (1280, 720)})
+picam2 = Picamera2()
+
+config = picam2.create_video_configuration(main={"size": (640, 480)})
 picam2.configure(config)
 
-encoder = H264Encoder(10000000)
-output = FfmpegOutput("/mnt/dsm/temp/test-python-%s.mp4" % time.time())
 
 while True:
-    # 60초마다 영상을 끊고 파일을 저장하는 반복문
-    time.sleep(60)
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    filename = "test-python-{}.mp4".format(current_time)
+    encoder = H264Encoder(10000000)
+    output = FfmpegOutput(filename)
 
-    picam2.stop_recording()
+    # 60초마다 영상을 끊고 파일을 저장하는 반복문
+    
 
     picam2.start_recording(encoder, output )
+    time.sleep(15)
+
+    picam2.stop_recording()
+    print("recorded once" + filename )
+    
+    shutil.move(working_directory + "/" + filename, dst)
 
 picam.stop_recording()
 
